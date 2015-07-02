2d50dc1511c306fec1fb098062df22864a1397c9
Christine Wen <christinewen@christines-mbp.zyxel.com>
2015-07-01 18:02:55 -0700

    Initial Commit

---
+++ Cheapify 2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cheapify 2.0
+//
+//  Created by Christine Wen on 7/1/15.
+//  Copyright (c) 2015 Your Friend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Cheapify 2.0Tests/Cheapify_2_0Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Cheapify_2_0Tests.swift
+//  Cheapify 2.0Tests
+//
+//  Created by Christine Wen on 7/1/15.
+//  Copyright (c) 2015 Your Friend. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Cheapify_2_0Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
